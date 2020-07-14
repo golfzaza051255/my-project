aa4f06307c1f44b7625d2ecb9c4e23c50f9d1604
Golf-Fuu <golfzaza0512@gmail.com>
2020-07-14 22:08:42 +0700

    [Update] edit html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+// import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
+// import { HomeComponent } from './home/home.component';
+// import { RegisterComponent } from './register/register.component';
 import { BoxCardComponent } from './shares/component/box-card/box-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    HomeComponent,
-    RegisterComponent,
     BoxCardComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: '', redirectTo: '/home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
-      { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent },
     ]),
   ],
   providers: [],
